e0c321c906839ca0a784855a302b601ec1c7954a
orlandofury <orlandofury@gmail.com>
2013-10-05 00:40:00 -0600

    Mostrando Resultado en la lista

--- src/com/example/mpesoconsulta/MPesoCaller.java
+++ src/com/example/mpesoconsulta/MPesoCaller.java
@@ -3,9 +3,14 @@ package com.example.mpesoconsulta;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.graphics.Color;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request.Method;
@@ -22,32 +27,73 @@ public class MPesoCaller extends ContextWrapper {
 	}
 
 	private String url = "http://www.mpeso.net/datos/consulta.php";
-	//private String url = "http://190.124.38.36/datos/consulta.php";
 
-	public void consultarSaldo(final String tarjeta) {
+	public void consultarSaldo(final String tarjeta,final TextView txtResult) {
 		RequestQueue queue = Volley.newRequestQueue(this);
-		StringRequest request = new StringRequest(Method.POST, url, 
+		StringRequest request = new StringRequest(Method.POST, url,
 				new Response.Listener<String>() {
+
+					private MPSOResult mpsoresult;
 
 					@Override
 					public void onResponse(String response) {
+						
+						JSONObject jObj = null;
+						
+						try {
+							jObj = new JSONObject(response);
+						} catch (JSONException e) {
+							Log.d("JSONException Creating JSONObject",
+									e.getMessage());
+							e.printStackTrace();
+						}
+						
+						try {
+							if(jObj!=null)
+							{
+								mpsoresult = MPsoProcessor.process(
+										getBaseContext(), jObj,tarjeta);
+								Log.d("MPSO Caller Internal", "callback result: " + mpsoresult.toString());
+								//Toast.makeText(getBaseContext(), mpsoresult.toString(),
+								//		Toast.LENGTH_LONG).show();
+								Log.d("MPSO Caller Internal","callback setText before: " + txtResult.getText());
+								txtResult.setText(mpsoresult.toString());
+								Log.d("MPSO Caller Internal","callback setText after: " + txtResult.getText());
+							}
+							else
+							{
+								Log.e("MPSO Caller - Error Response","Error Response:" + response.length());
+								if(response.length() == 0)
+								{
+									txtResult.setTextColor(Color.parseColor("#FF0000"));
+									txtResult.setText("No es un Numero de tarjeta valida");
+								}
+								Log.d("MPSO Caller - Error Response", "This response must be treated different" + response.length());
+							}
+						} catch (JSONException e) {
+							Log.d("JSONException Processor", e.getMessage());
+							e.printStackTrace();
+						}
 						Log.d("MPESOAPP", response);
+						
+						
+
+						// TODO:Llamar al setItem
+
 					}
-				},new Response.ErrorListener() {
+				}, new Response.ErrorListener() {
 
 					@Override
 					public void onErrorResponse(VolleyError error) {
-						Log.d("DEBUG","ERROR - " + error.getMessage());
+						Log.d("DEBUG", "ERROR - " + error.getMessage());
 					}
-				}
-				) { 
+				}) {
 			@Override
-			protected Map<String, String> getParams()
-					throws AuthFailureError {
+			protected Map<String, String> getParams() throws AuthFailureError {
 				// TODO Auto-generated method stub
-				Map<String, String>  params = new HashMap<String, String>();  
-	            params.put("_funcion", "1");  
-	            params.put("_terminal", tarjeta);
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("_funcion", "1");
+				params.put("_terminal", tarjeta);
 				return params;
 			}
 		};
--- src/com/example/mpesoconsulta/MPsoActivity.java
+++ src/com/example/mpesoconsulta/MPsoActivity.java
@@ -1,6 +1,7 @@
 package com.example.mpesoconsulta;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -8,28 +9,49 @@ import android.view.Menu;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MPsoActivity extends Activity {
-	
+	//private MPSOAdapter adapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mpso);
-		final EditText editText = (EditText)findViewById(R.id.editTUC);
-		
+		final EditText editText = (EditText) findViewById(R.id.editTUC);
+		final TextView txtResult = (TextView) findViewById(R.id.textResult);
+		//TODO: Implementar retorno de valores a una lista cuando se vuelva a consultar utilizando el adaptar
+		//ListView list = (ListView) findViewById(R.id.list);
+		//adapter = new MPSOAdapter(this);
+		//list.setAdapter(adapter);
 		editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-			
+
 			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if(actionId == EditorInfo.IME_ACTION_DONE)
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					String tarjetaTUC = editText.getText().toString();
+					MPesoCaller mpsoCaller = new MPesoCaller(getBaseContext());
+					
+					if(editText.getText().length()<8 || editText.getText().length()>8)
 					{
-						String tarjetaTUC = editText.getText().toString();
-					 	new MPesoCaller(getBaseContext()).consultarSaldo(tarjetaTUC);
-						Log.d("MPESOAPP", "On Editor Action");
-						return true;
+						txtResult.setTextColor(Color.parseColor("#FF0000"));
+						txtResult.setText("No es un Número de tarjeta válida");
 					}
-				
+					else
+					{
+						
+						if(editText.getText().length() == 8)
+						{
+							//TODO: Feedback al usuario que esta trabajando??
+							mpsoCaller.consultarSaldo(tarjetaTUC,txtResult);
+						}
+							
+					}
+					
+					Log.d("MPESOAPP", "On Editor Action");
+					return true;
+				}
+
 				return false;
 			}
 		});
